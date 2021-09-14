17823086c8af6b31613add037a7ad1702bb2772e
HublinTom <72757828+HublinTom@users.noreply.github.com>
2021-09-14 16:26:33 +0700

    Change in bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[55], G[5], answer[10];
+{ char Name[55], G[5], answer[20];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your Name please? ");
     scanf("%s", Name);
     do
     {
